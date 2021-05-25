e2bb97fc02381ac31fa0b1b240feff37491d89f8
Ententa_PL <84442980+EntentaPL@users.noreply.github.com>
2021-05-25 19:45:32 +0200

    najwieksza_liczba.py
    File.

---
+++ pytania_i_odpowiedzi.py
@@ -0,0 +1,23 @@
+input('Wprowadź numer1')
+a = input('')
+input('WProwadź numer 2')
+b = input('')
+input ('WProwadź numer 3')
+c = input('')
+
+
+if a > b :
+    v = (a)
+else:
+    v = (b)
+if b > v :
+    v = (b)
+else:
+     v = (v)
+
+if c > v :
+    v = (c)
+else:
+ v = (v)
+
+print('Największą liczbą jest ' + v )
